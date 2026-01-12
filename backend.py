66da255ed444514572b22a04b6ad03086900a956
pawan-xdd <manjhipawankumar26@gmail.com>
2026-01-12 22:14:47 +0530

    Fix temp unit, add Sky condition based on API data with date and time

--- backend.py
+++ backend.py
@@ -6,7 +6,7 @@ load_dotenv()
 WEATHER_API = os.getenv("API_KEY")
 
 
-def get_data(place,forecast_days, kind):
+def get_data(place,forecast_days):
 
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={WEATHER_API}"
     response = requests.get(url)
@@ -14,12 +14,8 @@ def get_data(place,forecast_days, kind):
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="Bhilai", forecast_days=6, kind="Temperature"))
+    print(get_data(place="Bhilai", forecast_days=6))
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import plotly.express as px
 from backend import get_data
+import datetime
 
 st.title("Weather Forecast for the next the days")
 place = st.text_input("Place: ")
@@ -12,11 +13,35 @@ option = st.selectbox("Select data to view:",
 
 st.subheader(f"{option} for the next {days} days in {place.title()}")
 
+try:
+    if place:
+        filtered_data = get_data(place, days)
 
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"]- 273.15 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates,y=temperatures,labels={"x": "Date", "y": "Temperatures (in °C)."})
+            st.plotly_chart(figure)
 
-dates, temperatures = get_data(place,days,option)
+        if option == "Sky":
+            # images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+            #           "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            # sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            # image_path = [images[sky_condition] for sky_condition in sky_conditions]
 
-figure = px.line(x=dates,y=temperatures,labels={"x": "Date", "y": "Temperatures (in °C)."})
-st.plotly_chart(figure)
+            api_data = get_data(place, days)
+            dates = [date["dt_txt"] for date in api_data]
+            sky_condition = [f"images/{data['weather'][0]['main']}.png" for data in api_data]
+            display_dates = [i.split('-') for i in dates]
+            out = []
+            for i in range(len(dates)):
+                out.append(datetime.datetime(int(display_dates[i][0]), int(display_dates[i][1]),
+                                             int(display_dates[i][2][:2]), int(display_dates[i][2][3:5]), 0))
+            final = [x.strftime("%a, %b %d %H:%M") for x in out]
+            st.image(sky_condition, width=175, caption=final)
 
+            # st.image(image_path, width=115)
+
+except KeyError:
+    st.warning("No such place!", icon="⚠️")
 
